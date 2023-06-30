00c48a91d29bc7a99b6d0b7fb8fdc68d820e3c55
horockey <rassudov99@yandex.ru>
2023-06-30 21:56:05 +0300

    docs: docstrings

--- event_headers.go
+++ event_headers.go
@@ -2,10 +2,12 @@ package scheduler
 
 import "github.com/horockey/go-scheduler/internal/model"
 
+// Get standart header for event ID.
 func EventHeaderID() string {
 	return model.HeaderID
 }
 
+// Get standart header for event creation time.
 func EventHeaderCreatedAt() string {
 	return model.HeaderCreatedAt
 }
